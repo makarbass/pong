25c4cc0f9e8479b705fe2e6e5b8deb3f18964143
makarbass <52400119+makarbass@users.noreply.github.com>
2021-10-19 14:52:57 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,77 +1,200 @@
-#include "fdf.h"
+#include "mlx.h"
+#include <stdlib.h> 
 
-int render_next_frame(fdf *data) // РЕНДЕРИНГ СЛЕДУЮЩЕГО КАДРА
+typedef struct // та самая структура, в которой хранятся все переменные
 {
-	//mlx_clear_window (data->mlx_ptr, data->win_ptr); // ЧИСТИМ КАДР
-//	square(data, 0x00ff0000); // РИСУЕМ НОВЫЙ КВАДРАТ
-	rectangle(data, 5, 20 ,0x00ffffff); 
-	return(0);
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+	int x3;
+	int y3;
+	int zoom;
+	int width;
+	int height;
+	void *mlx_ptr;
+	void *win_ptr;
+}	fdf;
+
+int	close(int keycode, fdf *data)
+{
+	exit(0);
 }
 
-int	close_app(int keycode, fdf *data) //НАЖАТИЕ НА х
+void horizontal_line (int x, int x1, int y, fdf *data)
 {
-	exit(0); 
+	while(x < x1)
+	{
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, 0x00ffffff);	
+		x++;
+	}
 }
 
-int resize(int keycode, fdf *data) // СВОРАЧИВАНИЕ & РАЗВОРОТ ОКНА
+void vertical_line (int x, int y, int y1, fdf *data)
 {
-//	printf("RESIZE\n");
-	return(0);
+	while(y < y1)
+	{
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, 0x00ffffff);
+		y++;
+	}
+}
+
+void square(fdf *data, int color) // для рисования квадрата передаем структуру
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (x < data->zoom) // пока Х меньше коэфицента зума
+	{
+		y = 0; // обнуляем y (возвращаемся в начало строки)
+		while(y < data->zoom) // пока Y меньше коэфицента зума
+		{
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + data->x1, y + data->y1, color); // рисуем точку
+			y++; // смещаемся по строке
+		}
+		x++; // переходим на следующую строку
+	}
+}
+
+void square2(fdf *data, int color) // для рисования квадрата передаем структуру
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (x < data->zoom) // пока Х меньше коэфицента зума
+	{
+		y = 0; // обнуляем y (возвращаемся в начало строки)
+		while(y < data->zoom) // пока Y меньше коэфицента зума
+		{
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + data->x2, y + data->y2, color); // рисуем точку
+			y++; // смещаемся по строке
+		}
+		x++; // переходим на следующую строку
+	}
+}
+
+void square3(fdf *data, int color) // для рисования квадрата передаем структуру
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (x < data->zoom) // пока Х меньше коэфицента зума
+	{
+		y = 0; // обнуляем y (возвращаемся в начало строки)
+		while(y < data->zoom) // пока Y меньше коэфицента зума
+		{
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + data->x3, y + data->y3, color); // рисуем точку
+			y++; // смещаемся по строке
+		}
+		x++; // переходим на следующую строку
+	}
+}
+
+
+void rectangle(fdf *data, int color) // ЛЕВАЯ РАКЕТКА
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (x < 4) // пока Х меньше коэфицента зума
+	{
+		y = 0; // обнуляем y (возвращаемся в начало строки)
+		while(y < 32) // пока Y меньше коэфицента зума
+		{
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + data->x1, y + data->y1, color); // рисуем точку
+			y++; // смещаемся по строке
+		}
+		x++; // переходим на следующую строку
+	}
+}
+
+void rectangle2(fdf *data, int color) // ПРАВАЯ РАКЕТКА
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (x < 4) // пока Х меньше коэфицента зума
+	{
+		y = 0; // обнуляем y (возвращаемся в начало строки)
+		while(y < 32) // пока Y меньше коэфицента зума
+		{
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + data->x2, y + data->y2, color); // рисуем точку
+			y++; // смещаемся по строке
+		}
+		x++; // переходим на следующую строку
+	}
 }
 
 int deal_key(int key, fdf *data)  // НАЖАТИЕ КНОПКИ
 {
-//	printf("keycode : %d\n", key);
+	printf("keycode : %d\n", key);
 	if (key == 53) // ESC
+	{
 		exit(0); // выйти из программы
-	if ((key == 123 || key == 0) && (data->x > data->zoom)) // <- & A и контроль за тем, чтоб квадрат не вышел за рамку
+	}
+//	if ((key == 123 || key == 0) && (data->x > data->zoom)) // <- & A и контроль за тем, чтоб квадрат не вышел за рамку
+//	{
+//		square(data, 0x00000000); // на месте старого квадрата рисуем черный квадрат
+//		data->x -= data->zoom; // смещаемся по х влево
+//	}
+//	if ((key == 124 || key == 2) && (data->x < (data->width - 2 * data->zoom))) // -> & D и находимся не ближе чем край рамки минус один квадрат (два квадрата от края окна)
+//	{	
+//		square(data, 0x00000000); // на месте старого квадрата рисуем черный квадрат
+//		data->x += data->zoom; // смещаемся по х вправо
+//	}
+	if ((key == 1) && (data->y1 < (data->height - 2 * data->zoom - 28)))// DOWN & S  || key == 1
 	{
 		square(data, 0x00000000); // на месте старого квадрата рисуем черный квадрат
-		data->x -= data->zoom; // смещаемся по х влево
+		data->y1 += data->zoom; // смещаемся по у вниз
 	}
-	if ((key == 124 || key == 2) && (data->x < (data->width - 2 * data->zoom))) // -> & D и находимся не ближе чем край рамки минус один квадрат (два квадрата от края окна)
-	{	
-		square(data, 0x00000000); // на месте старого квадрата рисуем черный квадрат
-		data->x += data->zoom; // смещаемся по х вправо
-	}
-	if ((key == 125 || key == 1) && (data->y + 20 < (data->height - 19 * data->zoom + 2)))// DOWN & S
+	if ((key == 13) && (data->y1 > data->zoom))//UP & W  || key == 13
 	{
-		//square(data, 0x00000000); // на месте старого квадрата рисуем черный квадрат
-		rectangle(data, 5, data->zoom ,0x00000000);
-		data->y += data->zoom * 3; // смещаемся по у вниз
-
+		data->y1 += 28; // 28 - это 32(высота ракетки) минус zoom
+		square(data, 0x00000000); // стираем квадрат равный zoom внизу прямоугольника
+		data->y1 -= 28 + data->zoom; // смещаемся по у вверх
 	}
-	if ((key == 126 || key == 13) && (data->y > 3 * data->zoom - 6))//UP & W
+	if ((key == 125) && (data->y2 < (data->height - 2 * data->zoom - 28)))  // DOWN
 	{
-		//square(data, 0x00000000); // на месте старого квадрата рисуем черный квадрат
-		//rectangle(data, 5, data->y + data->zoom + 20 ,0x00000000);
-		data->y -= data->zoom * 3; // смещаемся по у вверх
-		rectangle(data, 5, data->y ,0x00000000);
-		
-		
+		square2(data, 0x00000000); // на месте старого квадрата рисуем черный квадрат
+		data->y2 += data->zoom; // смещаемся по у вниз
 	}
-	return(0);
-}
-
-int release_key(int key, fdf *data) //ОТПУСК КНОПКИ
-{
-	//printf("OTPUSK\n");
-	return(0);
+	if ((key == 126) && (data->y2 > data->zoom)) // UP
+	{
+		data->y2 += 28; // 28 - это 32(высота ракетки) минус zoom
+		square2(data, 0x00000000); // стираем квадрат равный zoom внизу прямоугольника
+		data->y2 -= 28 + data->zoom; // смещаемся по у вверх
+	}
 }
 
 void init(fdf *data) // запись начальных значений
 {
-	data->width = 420;
-	data->height = 300;
+	data->width = 320;
+	data->height = 200;
 	data->mlx_ptr = mlx_init(); // инизиацизируем форму (значение в структуре data - mlx_ptr) 
 	data->win_ptr = mlx_new_window(data->mlx_ptr, data->width, data->height, "TEST"); // создаем окно 
 	data->zoom = 4; // коэффицент размера (будет не пиксель а квадрат 16х16)
-	data->x = 10*2; // начальная точка х
-	data->y = 30; // начальная точка у
-	data->x_back = data->x;
-	data->y_back = data->y;
-//	square(data, 0x00FF0000); // рисование квадрата
-	rectangle(data, 5, 20 ,0x00ffffff);
+	data->x1 = 16; // начальная точка х
+	data->y1 = 16; // начальная точка у
+	data->x2 = data->width - 16 - data->zoom; // начальная точка х
+	data->y2 = 16; // начальная точка у
+	data->x3 = 158;
+	data->y3 = 98;
+	square3(data, 0x00FFFFFF); // рисование квадрата
+	rectangle(data, 0x00FFFFFF);
+	rectangle2(data, 0x00FFFFFF);
+}
+
+int render_next_frame(fdf *data) // РЕНДЕРИНГ СЛЕДУЮЩЕГО КАДРА
+{
+	//mlx_clear_window (data->mlx_ptr, data->win_ptr); // ЧИСТИМ КАДР
+	//square(data, 0x00FFFFFF); // РИСУЕМ НОВЫЙ КВАДРАТ
+	rectangle(data, 0x00FFFFFF);
+	rectangle2(data, 0x00FFFFFF);
 }
 
 void ramka(fdf *data) // рисуем рамку
@@ -82,25 +205,18 @@ void ramka(fdf *data) // рисуем рамку
 	vertical_line((data->width - data->zoom), (data->zoom - 1), (data->height - data->zoom + 1), data);
 }
 
-int main(int argc, char** argv)
+int	main(void)
 {
-	fdf *data; // структура, в которой хранятся все данные вместо переменных
+	fdf	*data;
 
-	data = (fdf*)malloc(sizeof(fdf)); // Выделяем память
-	init(data); // записываем начальные значения
+	data = (fdf*)malloc(sizeof(fdf));
+	init(data);
 	ramka(data); // рисуем рамку
+	
+	mlx_hook(data->win_ptr, 2, 1<<3, deal_key, data); 
+	mlx_hook(data->win_ptr, 17, 0, close, data);
 
-	mlx_hook(data->win_ptr, 2, 0, deal_key, data); // следим за кнопками ( передаем окно, функцию deal_key и структуру(для изменения значений в ней))
-	mlx_hook(data->win_ptr, 3 ,0, release_key, data); // отпустил кнопку
-	mlx_hook(data->win_ptr, 17, 0, close_app, data); // НАЖАТИЕ НА "x" (крестик для закрытия)
-	mlx_hook(data->win_ptr, 12, 0, resize, data); // развернул окно или свернул
-
-	//mlx_string_put(data->mlx_ptr, data->win_ptr, 75, 110, 0xFFFFFF, "PRESS SPACE");
-	mlx_loop_hook(data->mlx_ptr, render_next_frame, data); // СЛЕДУЮЩИЙ КАДР
-	mlx_loop(data->mlx_ptr); // держим окно открытым пока программа работает 
-	free(data); // освобождаем память
-	return(0);
+	mlx_loop_hook(data->mlx_ptr, render_next_frame, data); 
+	mlx_loop(data->mlx_ptr);
+	free(data);
 }
-
-//	bresenham(16, 16, 784, 16, data);  //рисование линии
-//	mlx_pixel_put(data->mlx_ptr, data->win_ptr, data->x, data->y, 0x00FF0000); // рисование пикселя
